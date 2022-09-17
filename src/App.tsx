87405a35ee491431471500da5348543906a2ec6b
Alex <alex.zhelyazko@yandex.by>
2022-09-17 23:52:20 +0400

    add; route for catalog component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import MainPage from './Layouts/MainPage';
 import './App.scss';
+import Catalog from './components/Catalog/Catalog';
 
 export const App: React.FC = () => {
   return (
     <div>
-      <MainPage />
+      <Routes>
+        <Route path='/' element={<MainPage/>}/>
+        <Route path='/man' element={<Catalog/>}/>
+        <Route path='/woman' element={<Catalog/>}/>
+        <Route path='/children' element={<Catalog/>}/>
+      </Routes>
     </div>
   );
 };
