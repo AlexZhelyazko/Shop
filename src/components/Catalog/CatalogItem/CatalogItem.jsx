b456c048b7e6b764f5ac5e9144f977e1870ca7d9
Alex <alex.zhelyazko@yandex.by>
2022-11-25 18:21:53 +0400

    catalogItem

--- src/components/Catalog/CatalogItem/CatalogItem.jsx
+++ src/components/Catalog/CatalogItem/CatalogItem.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { fetchCatalogItems } from '../../../redux/catalog/asyncActions';
+import { useAppDispatch } from '../../../redux/store';
+import './catalogItem.scss';
 
 function CatalogItem() {
-  return <div>CatalogItem</div>;
+  const dispatch = useAppDispatch();
+  const params = useParams();
+  const items = useSelector((state) => state.catalog.items);
+  const currentItem = items.find((el) => el.id === params.id);
+  useEffect(() => {
+    dispatch(fetchCatalogItems());
+  }, []);
+  console.log(currentItem);
+  return (
+    <div className="catalogItem__wrapper">
+      <div className="catalogItem__image">
+        <img src={currentItem.frontImageUrl} alt="" />
+      </div>
+      <div className="catalogItem__info">
+        <div>{currentItem.price}</div>
+        <div>{currentItem.title}</div>
+        <div></div>
+      </div>
+    </div>
+  );
 }
 
 export default CatalogItem;
