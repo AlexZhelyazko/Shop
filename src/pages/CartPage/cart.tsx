345ad5ec6961e115cdeb42e267e34c58875d9599
Alex <alex.zhelyazko@yandex.by>
2023-01-25 06:14:44 +0400

    cart page

--- src/pages/CartPage/cart.tsx
+++ src/pages/CartPage/cart.tsx
@@ -7,26 +7,29 @@ export default function Cart() {
   const cartItems = useSelector((state: RootState) => state.cart.cartItems);
   const dispatch = useAppDispatch();
 
+  if (cartItems.length === 0) {
+    return <h1>Empty</h1>;
+  }
   return (
     <div className="cart__container">
-      {cartItems.length === 0 ? (
-        <h1>Empty</h1>
-      ) : (
-        cartItems.map((el, index) => {
-          return (
-            <div key={el.count + el.title + index} className="cart__item">
-              <h1>Your Cart</h1>
-              <img src={el.img} alt="" />
+      <h1 style={{ fontFamily: 'initial', fontWeight: 'bold', fontSize: '35px' }}>YOUR CART</h1>
+      {cartItems.map((el, index) => {
+        return (
+          <div key={el.title + index} className="cart__item">
+            <img src={el.img} alt="" />
+            <div className="cart__item-info">
               <h2>{el.title}</h2>
-              <h2>Size: {el.size}</h2>
-              <h2>Count: {el.count}</h2>
-              <button onClick={() => dispatch(deleteItemfromCart(el.title))}>delete</button>
-              <button onClick={() => dispatch(addItem(el.title))}>add</button>
-              <button onClick={() => dispatch(minusItem(el.title))}>minus</button>
+              <div>
+                <h2>SIZE: {el.size}</h2>
+                <h2>COUNT: {el.count}</h2>
+              </div>
             </div>
-          );
-        })
-      )}
+            <button onClick={() => dispatch(deleteItemfromCart(el.title))}>delete</button>
+            <button onClick={() => dispatch(addItem(el.title))}>add</button>
+            <button onClick={() => dispatch(minusItem(el.title))}>minus</button>
+          </div>
+        );
+      })}
     </div>
   );
 }
