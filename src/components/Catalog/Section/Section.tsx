428e02ab1bcf3fed506c2aeee750707543e4e3e8
Alex <alex.zhelyazko@yandex.by>
2023-02-05 13:53:45 +0400

    update: section

--- src/components/Catalog/Section/Section.tsx
+++ src/components/Catalog/Section/Section.tsx
@@ -25,44 +25,50 @@ const Section: React.FC<SectionProps> = ({ items }) => {
 
   const fakeArr = [...new Array(8)];
 
-  return notFoundItems ? (
-    <NotFound />
-  ) : (
+  if (notFoundItems) {
+    return <NotFound />;
+  }
+
+  if (status === 'pending') {
+    return (
+      <>
+        {fakeArr.map(() => (
+          <div className="catalog__section-product">
+            <SkeletonLoader />
+          </div>
+        ))}
+      </>
+    );
+  }
+
+  return (
     <section
       className="catalog__section"
       style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
-      {status === 'pending'
-        ? fakeArr.map(() => (
+      {(filterItems.length === 0 && isFilterActive === false ? items : filterItems).map(
+        (item: IProduct) => {
+          return (
             <div className="catalog__section-product">
-              <SkeletonLoader />
-            </div>
-          ))
-        : (filterItems.length === 0 && isFilterActive === false ? items : filterItems).map(
-            (item: IProduct) => {
-              return (
-                <div className="catalog__section-product">
-                  <NavLink
-                    className="catalog__section-product catalog__section-link"
-                    to={`${item.id}`}>
-                    <img
-                      onMouseOver={(event) => handleMouseEnter(event, item?.frontImageUrl || '')}
-                      onMouseOut={(event) => handleMouseOut(event, item?.backImageUrl || '')}
-                      className="catalog__section-image"
-                      width="228px"
-                      height="300px"
-                      src={item.backImageUrl}
-                      alt={item.title}
-                    />
-                    <div className="catalog__section-product-info">
-                      <span className="catalog__section-product-title">{item.title}</span>
-                      <span>{item.size + ' '}</span>
-                      <span>{item.price} </span>
-                    </div>
-                  </NavLink>
+              <NavLink className="catalog__section-product catalog__section-link" to={`${item.id}`}>
+                <img
+                  onMouseOver={(event) => handleMouseEnter(event, item?.frontImageUrl || '')}
+                  onMouseOut={(event) => handleMouseOut(event, item?.backImageUrl || '')}
+                  className="catalog__section-image"
+                  width="228px"
+                  height="300px"
+                  src={item.backImageUrl}
+                  alt={item.title}
+                />
+                <div className="catalog__section-product-info">
+                  <span className="catalog__section-product-title">{item.title}</span>
+                  <span>{item.size + ' '}</span>
+                  <span>{item.price} </span>
                 </div>
-              );
-            },
-          )}
+              </NavLink>
+            </div>
+          );
+        },
+      )}
     </section>
   );
 };
