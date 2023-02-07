1219eea4f579075bd64e4bd8e9d77d34920b5395
Alex <alex.zhelyazko@yandex.by>
2023-02-07 17:23:05 +0400

    .

--- src/redux/auth/authSlice.ts
+++ src/redux/auth/authSlice.ts
@@ -1,6 +1,7 @@
 import { PayloadAction } from '@reduxjs/toolkit';
 import { IUser } from './../../@types/types';
 import { createSlice } from '@reduxjs/toolkit';
+import { PURGE } from 'redux-persist';
 interface IAuth {
     isAuth: boolean,
     currentUser: any
@@ -29,7 +30,12 @@ const auhtSlice = createSlice({
         setCurrentUser(state, action: PayloadAction<IUser | {}>) {
             state.currentUser = action.payload
         }
-    }
+    },
+    // extraReducers: (builder) => {
+    //     builder.addCase(PURGE, (state) => {
+    //         customEntityAdapter.removeAll(state);
+    //     });
+    // }
 })
 
 export const {setIsAuth, setCurrentUser} = auhtSlice.actions
