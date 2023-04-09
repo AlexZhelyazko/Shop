27f831be14ef6201cea71a714160ad157928fee3
Alex <alex.sionis846@gmail.com>
2023-04-09 16:26:45 +0300

    ,

--- src/pages/Orders/Orders.tsx
+++ src/pages/Orders/Orders.tsx
@@ -2,6 +2,7 @@ import { queryApi } from "../../redux/query";
 
 const Orders = () => {
     const { data = [], isLoading, isError } = queryApi.useGetUsersQuery();
+    console.log(data)
     return (
         <div>
             Orders
