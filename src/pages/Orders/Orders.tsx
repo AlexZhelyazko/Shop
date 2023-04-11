4894e8e109291860f1afecf880f1e7e095f0c43c
Alex <alex.sionis846@gmail.com>
2023-04-11 09:04:02 +0300

    orders

--- src/pages/Orders/Orders.tsx
+++ src/pages/Orders/Orders.tsx
@@ -5,14 +5,31 @@ const Orders = () => {
     let orders = data.filter((el) => {
         return el.hasOwnProperty("history") 
     })
+    console.log(orders)
     return (
         <div>
             <h2>Orders:</h2>
-            <div>{orders.map((el) => {
-                return Object.entries(el.history).map((order: any) => {
-                    return <div>{order[0]}</div>
-                })
-            })}</div>
+            <div>
+                {orders.map((el) => {
+                    return (
+                        <>
+                            <div>{el.name}</div>
+                            {Object.entries(el.history).map((order: any) => {
+                                return  (
+                                    <>
+                                        <div>{order[0]}</div>
+                                        <div>{order[1].status}</div>
+                                        {order[1].item.map((item: any) => (
+                                            <span>{item.title}</span>
+
+                                         ))}
+                                    </>
+                                )
+                            })}
+                        </>
+                    )
+                })}
+            </div>
         </div>
     )
 }
