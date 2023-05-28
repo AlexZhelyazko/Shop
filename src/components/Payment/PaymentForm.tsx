a182107e78de1a9c1b151616aa8a03183ae53d69
Alex <alex.sionis846@gmail.com>
2023-05-28 09:12:21 +0300

    .

--- src/components/Payment/PaymentForm.tsx
+++ src/components/Payment/PaymentForm.tsx
@@ -19,6 +19,7 @@ const PaymentForm: React.FC<PaymentFormProps> = ({
   totalPrice,
 }) => {
   const [cardNumber, setCardNumber] = useState("");
+  const [cardType, setCardType] = useState("far fa-credit-card");
   const [expiryDate, setExpiryDate] = useState("");
   const [cvc, setCvc] = useState("");
   const [name, setName] = useState("");
@@ -26,25 +27,54 @@ const PaymentForm: React.FC<PaymentFormProps> = ({
   const [addItemsToHistory, { isLoading, isError }] =
     queryApi.useConfirmDiliveryBasketMutation();
 
+  // const handleCardNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   let val = e.target.value;
+  //   let valArray = val.split(" ").join("").split("");
+  //   let valSpace = val.split("");
+
+  //   if (valSpace[valSpace.length - 1] == " ") {
+  //     var valSpaceN = valSpace.slice(0, -2);
+  //     val = valSpaceN.join("");
+  //     setCardNumber(val);
+  //     return;
+  //   }
+
+  //   if (isNaN(+valArray.join(""))) return;
+  //   if (valArray.length === 17) return;
+  //   if (valArray.length % 4 === 0 && valArray.length <= 15) {
+  //     setCardNumber(e.target.value + "  ");
+  //   } else {
+  //     setCardNumber(e.target.value);
+  //   }
+  // };
+
   const handleCardNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
-    let val = e.target.value;
-    let valArray = val.split(" ").join("").split("");
-    let valSpace = val.split("");
-
-    if (valSpace[valSpace.length - 1] == " ") {
-      var valSpaceN = valSpace.slice(0, -2);
-      val = valSpaceN.join("");
-      setCardNumber(val);
-      return;
+    var matches = e.currentTarget.value.match(/(\d+)/);
+    var cardno = "";
+    console.log(matches);
+    if (matches) {
+      cardno = e.currentTarget.value.split(" - ").join("");
     }
-
-    if (isNaN(+valArray.join(""))) return;
-    if (valArray.length === 17) return;
-    if (valArray.length % 4 === 0 && valArray.length <= 15) {
-      setCardNumber(e.target.value + "  ");
-    } else {
-      setCardNumber(e.target.value);
+    console.log(cardno);
+    var cardtype1 = cardType;
+    //var visa = /^(?:4[0-9]{16}(?:[0-9]{3})?)$/;
+    var visa = /^(?:4[0-9]{2}?)$/;
+    var mastercardRegEx = /^(?:5[1-5][0-9]{3})$/;
+    var amexpRegEx = /^(?:3[47][0-9]{3})$/;
+    var discovRegEx = /^(?:6(?:011|5[0-9][0-9])[0-9]{5})$/;
+    console.log(visa.test(cardno));
+    if (visa.test(cardno) === true) {
+      //eg:4651970022334445
+      cardtype1 = "far fa fa-3x fa-cc-visa  carddetail-cardtype";
+    } else if (mastercardRegEx.test(cardno) === true) {
+      cardtype1 = "far fa fa-3x fa-cc-mastercard carddetail-cardtype";
+    } else if (amexpRegEx.test(cardno) === true) {
+      cardtype1 = "far fa fa-3x fa-cc-amex carddetail-cardtype";
+    } else if (discovRegEx.test(cardno) === true) {
+      cardtype1 = "far fa fa-3x fa-cc-discover carddetail-cardtype";
     }
+    setCardNumber(cardno);
+    setCardType(cardtype1);
   };
 
   // const handleCardDate = (e: React.FormEvent<HTMLInputElement>) => {
@@ -114,6 +144,16 @@ const PaymentForm: React.FC<PaymentFormProps> = ({
     setCount(count + 1);
   };
 
+  const cc_format = (value: any) => {
+    const v = value.replace(/[^0-9]/gi, "").substr(0, 16);
+
+    const parts = [];
+    for (let i = 0; i < v.length; i += 4) {
+      parts.push(v.substr(i, 4));
+    }
+    return parts.length > 1 ? parts.join(" - ") : value;
+  };
+
   return (
     <form onSubmit={handleSubmit}>
       {error && <p style={{ color: "red" }}>{error}</p>}
@@ -124,10 +164,13 @@ const PaymentForm: React.FC<PaymentFormProps> = ({
         <input
           className="payment__input"
           type="text"
+          data-mask="0000 0000 0000 0000"
+          placeholder="XXXX-XXXX-XXXX-XXXX"
           id="cardNumber"
-          value={cardNumber}
+          value={cc_format(cardNumber)}
           onChange={(e) => handleCardNumber(e)}
         />
+        <i className={cardType} id="cardtype"></i>
       </div>
       <div>
         <label className="payment__label" htmlFor="expiryDate">
@@ -139,6 +182,11 @@ const PaymentForm: React.FC<PaymentFormProps> = ({
           id="expiryDate"
           value={expiryDate}
           onChange={(e) => handleCardDate(e)}
+          onKeyPress={(event) => {
+            if (!/[0-9]/.test(event.key)) {
+              event.preventDefault();
+            }
+          }}
         />
       </div>
       <div>
